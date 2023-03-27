b88b5739108eacb27fe4fd372a6484afe8a65042
Marcelo Cruz <marcelo.cruzf11@gmail.com>
2023-03-27 12:38:04 -0300

    app lista primeiro commit

---
+++ appListaCurso/app/src/main/java/devandroid/marcelo/appListaCurso/MainActivity.java
@@ -0,0 +1,14 @@
+package devandroid.marcelo.appListaCurso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
